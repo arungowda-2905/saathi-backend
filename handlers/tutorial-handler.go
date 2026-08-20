14c07385f77085248ce748ed8da994254aa8f533
aniketkaushik12 <aniket.kaushik@dmartlabs.com>
2026-08-20 10:25:56 +0530

    GetDetailsByRole updated

--- handlers/tutorial-handler.go
+++ handlers/tutorial-handler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 
 	"saathi-backend/config"
 	"saathi-backend/model"
@@ -110,4 +111,75 @@ func GetTutorialByID(c *fiber.Ctx) error {
 	}
 
 	return c.Status(fiber.StatusOK).JSON(tutorial)
+}
+
+func GetDetailsByRole(c *fiber.Ctx) error {
+
+	// Get roles from authentication middleware
+	userRole := c.Get("X-Role")
+
+	if userRole == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "User roles not found",
+		})
+	}
+
+	userRoles := []string{userRole}
+
+	if len(userRoles) == 0 {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "User has no roles",
+		})
+	}
+
+	// Find tutorials that match ANY of the user's roles
+	filter := bson.M{
+		"roles": bson.M{
+			"$in": userRoles,
+		},
+		"is_active": true,
+	}
+
+	// Only return the fields you need
+	projection := bson.M{
+		"_id":         1,
+		"video_title": 1,
+		"app_name":    1,
+		"video_link":  1,
+		"created_at":  1,
+	}
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		10*time.Second,
+	)
+	defer cancel()
+
+	cursor, err := config.DB.
+		Collection("tutorials").
+		Find(
+			ctx,
+			filter,
+			options.Find().SetProjection(projection),
+		)
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to fetch tutorials",
+		})
+	}
+
+	defer cursor.Close(ctx)
+
+	var tutorials []model.Tutorial
+
+	if err := cursor.All(ctx, &tutorials); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to decode tutorials",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"data": tutorials,
+	})
 }
