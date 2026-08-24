bab676116144128945fe34f86836104747458476
Prateek Bhat <prateek.bhat@dmartlabs.com>
2026-08-24 14:42:56 +0530

    api updated

--- routes/routes.go
+++ routes/routes.go
@@ -11,7 +11,7 @@ func SetupRoutes(app *fiber.App, tutorialHandler *handlers.TutorialHandler) {
 	api := app.Group("/saathi/api")
 
 	api.Post("/v1/upload", tutorialHandler.UploadVideo)
-
+	api.Post("/videos/v1", handlers.HandleVideoUpload)
 	api.Get("/v1/:videoId", handlers.GetTutorialByID)
 	api.Get("/details/v1", handlers.GetDetailsByRole)
 }
