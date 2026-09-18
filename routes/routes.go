c1a12ff00e7517e2657116c0f2255b43bc0d5257
aniketkaushik12 <aniket.kaushik@dmartlabs.com>
2026-09-18 17:04:23 +0530

    updates

--- routes/routes.go
+++ routes/routes.go
@@ -13,7 +13,10 @@ func SetupRoutes(app *fiber.App, tutorialHandler *handlers.TutorialHandler, tuto
 	api.Post("/v1/upload", tutorialHandler.UploadVideo)
 	api.Post("/v1/tutorials", tutorialHandler.CreateTutorial)
 	api.Post("/v1/translations", tutorialHandler.CreateTranslation)
+	
 	api.Get("/v1/details", tutorialHandler.GetDetailsByRole)
 	api.Get("/v1/:videoId", tutorialHandler1.GetVideo)
 	api.Get("/v1/thumbnail/:thumbnailId", tutorialHandler1.GetTutorialThumbnailByID)
+	api.Get("/search/v1", tutorialHandler.SearchTutorials)
+
 }
